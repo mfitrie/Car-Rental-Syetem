0c3c7b8d111e1bfb3ceea16aa47d59d4b4909092
mfitrie <mfitrie78@gmail.com>
2022-01-22 23:52:39 +0800

    refactor the folder

--- rentalPage/scriptRental.js
+++ rentalPage/scriptRental.js
@@ -15,7 +15,7 @@ const buttonConfirmRent = document.querySelector('.main_rentalFormContainer_rent
 // const paymentContainer = document.querySelector('.main_div_paymentContainer');
 
 // DATA CAR
-const listCarImage = ["Car_Image/honda_civic.png", "Car_Image/ford_mustang.png", "Car_Image/nissan-gtr.png", "Car_Image/honda_jazz.png", "Car_Image/toyota_vios.png", "Car_Image/perodua_aruz.png", "Car_Image/perodua_myvi.png"];
+const listCarImage = ["/Car_Image/honda_civic.png", "/Car_Image/ford_mustang.png", "/Car_Image/nissan-gtr.png", "/Car_Image/honda_jazz.png", "/Car_Image/toyota_vios.png", "/Car_Image/perodua_aruz.png", "/Car_Image/perodua_myvi.png"];
 const listCarName = ["Honda Civic", "Ford Mustang", "Nissan GTR", "Honda Jazz", "Toyota Vios", "Perodua Aruz", "Perodua Myvi"];
 
 // ICON
